37a8a168318166e63d591eed0bd0b1aa0d87e733
mcgeorgiev <michael_georgiev@hotmail.co.uk>
2016-10-04 17:04:57 +0100

    fixed threading issue, issue with one query

--- alt_airport.py
+++ alt_airport.py
@@ -1,4 +1,4 @@
-from connection import db
+from connection import Connection
 from math import pow, sqrt
 import operator
 import json
@@ -9,27 +9,31 @@ import json
 # closeness
 
 def get_lat_lon(iata_code):
-    iata = (iata_code,)
-    db.query(
-        """
+    with Connection() as conn:
+        cur = conn.cursor()
+        iata = (iata_code,)
+        cur.execute(
+            """
             SELECT latitude_deg, longitude_deg
             FROM airports
             WHERE iata_code = ?
         """, iata)
-    return db.fetchall()
+        return cur.fetchall()[0]
 
 
 def get_near_airports(lat, lon):
     # don't hard code distance
-    lat_lon = (lat, lat, lon, lon)
-    db.query(
+    with Connection() as conn:
+        cur = conn.cursor()
+        lat_lon = (lat, lat, lon, lon)
+        cur.execute(
         """
         select latitude_deg, longitude_deg, iata_code, name
         from airports
         where latitude_deg > (? - 1.3) and latitude_deg < (? + 1.3)
         and longitude_deg > (?-1.3) and longitude_deg < (?+1.3)
         """, lat_lon)
-    return [((item[0], item[1]), item[2], item[3]) for item in db.fetchall()]
+        return [((item[0], item[1]), item[2], item[3]) for item in cur.fetchall()]
 
 
 def format_airports(airports_data):
@@ -66,9 +70,26 @@ def get_k_nearest_neighbour(test, train, k=999):
 
 
 def check_routes(source, dest):
-    db.query(
-        "select airline, source_airport, destination_airport from routes where source_airport in (" + ",".join("?"*len(source)) + ") and destination_airport in (" + ",".join("?"*len(dest)) + ")", (source + dest))
-    return db.fetchall()
+    def log_and_execute(cursor, sql, *args):
+        print sql
+        s = sql
+        if len(args) > 0:
+            # generates SELECT quote(?), quote(?), ...
+            cursor.execute("SELECT " + ", ".join(["quote(?)" for i in args]), args)
+            quoted_values = cursor.fetchone()
+            for quoted_value in quoted_values:
+                s = s.replace('?', quoted_value, 1)
+        print "SQL command: " + s
+        cursor.execute(sql, args)
+
+    with Connection() as conn:
+        cur = conn.cursor()
+        query = "select airline, source_airport, destination_airport from routes where source_airport in (" + ",".join("?"*len(source)) + ") and destination_airport in (" + ",".join("?"*len(dest)) + ")"
+        log_and_execute(cur, query, (source + dest))
+        cur.execute(
+            "select airline, source_airport, destination_airport from routes where source_airport in (" + ",".join("?"*len(source)) + ") and destination_airport in (" + ",".join("?"*len(dest)) + ")", (source + dest))
+        data =  cur.fetchall()
+    return data
 
 
 def verify_routes(valid_routes, nearest_source, nearest_dest):
@@ -109,42 +130,36 @@ def create_json(source, source_lat, source_lon, dest, dest_lat, dest_lon, valid_
 def get_routes(input):
     # get lat lon by whatever means
     source_entry = input['source']
-    # source_lat, source_lon = get_lat_lon(source_entry)
-    import sqlite3
-    db = sqlite3.connect('airportdata.db')
-    cur = db.cursor()
-    cur.execute("select * from airports limit 2")
-    print cur.fetchall()
-    db.close()
-    # # get close airports
-    # source_airports = format_airports(get_near_airports(source_lat, source_lon))
+    source_lat, source_lon = get_lat_lon(source_entry)
 
-    # # order them
-    # nearest_source = get_k_nearest_neighbour((source_lat,source_lon), source_airports)
+    # get close airports
+    source_airports = format_airports(get_near_airports(source_lat, source_lon))
 
+    # order them
+    nearest_source = get_k_nearest_neighbour((source_lat,source_lon), source_airports)
 
-    # # get lat lon by whatever means
-    # dest_entry = input['distance']
-    # dest_lat, dest_lon = get_lat_lon(dest_entry)
+    # get lat lon by whatever means
+    dest_entry = input['destination']
+    dest_lat, dest_lon = get_lat_lon(dest_entry)
 
-    # # get close airports
-    # dest_airports = format_airports(get_near_airports(dest_lat, dest_lon))
+    # get close airports
+    dest_airports = format_airports(get_near_airports(dest_lat, dest_lon))
 
-    # # order them
-    # nearest_dest = get_k_nearest_neighbour((dest_lat,dest_lon), dest_airports)
+    # order them
+    nearest_dest = get_k_nearest_neighbour((dest_lat,dest_lon), dest_airports)
 
 
-    # # check if routes between them exist
-    # source_iata_list = [source['iata'] for source in nearest_source]
-    # dest_iata_list = [dest['iata'] for dest in nearest_dest]
-    # valid_routes_codes = check_routes(source_iata_list, dest_iata_list)
+    # check if routes between them exist
+    source_iata_list = [source['iata'] for source in nearest_source]
+    dest_iata_list = [dest['iata'] for dest in nearest_dest]
+    valid_routes_codes = check_routes(source_iata_list, dest_iata_list)
+    print '**********'
+    print valid_routes_codes
+    # verify_routes
+    valid_routes = verify_routes(valid_routes_codes, nearest_source, nearest_dest)
 
-    # # verify_routes
-    # valid_routes = verify_routes(valid_routes_codes, nearest_source, nearest_dest)
-
-    # # create json
-    # json_dict = create_json(source_entry, source_lat, source_lon, dest_entry, dest_lat, dest_lon, valid_routes)
-    # JSON = json.dumps(json_dict)
-    JSON = {"stuck": "here"}
+    # create json
+    json_dict = create_json(source_entry, source_lat, source_lon, dest_entry, dest_lat, dest_lon, valid_routes)
+    JSON = json.dumps(json_dict)
     return JSON
 
--- connection.py
+++ connection.py
@@ -1,21 +1,34 @@
 import sqlite3
 
+# class Connection(object):
+#     _db_connection = None
+#     _db_cur = None
+
+#     def __init__(self):
+#         self._db_connection = sqlite3.connect('airportdata.db')
+#         self._db_cur = self._db_connection.cursor()
+
+#     def query(self, query, param):
+#         return self._db_cur.execute(query, param)
+
+#     def fetchall(self):
+#         return self._db_cur.fetchall()
+
+#     def __del__(self):
+#        self._db_connection.close()
+
 class Connection(object):
-    _db_connection = None
-    _db_cur = None
-
     def __init__(self):
-        self._db_connection = sqlite3.connect('airportdata.db')
-        self._db_cur = self._db_connection.cursor()
+        self.db_name = 'airportdata.db'
 
-    def query(self, query, param):
-        return self._db_cur.execute(query, param)
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
 
-    def fetchall(self):
-        return self._db_cur.fetchall()
+    def __exit__(self, type, value, traceback):
+        self.conn.close()
 
-    def __del__(self):
-        self._db_connection.close()
 
-db = Connection()
+
+# db = Connection()
 
