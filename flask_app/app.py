99d429b08117775478d356829114402fbfb6e098
mcgeorgiev <michael_georgiev@hotmail.co.uk>
2016-10-13 16:37:25 +0100

    basic flask frontend connection

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,18 +1,21 @@
-from flask import Flask
-from flask import request
-from flask import render_template
+from flask import Flask, request, render_template, jsonify
+import requests
 
+api_url = "http://127.0.0.1:5000/post"
 app = Flask(__name__)
 
 @app.route('/')
-def my_form():
+def home():
     return render_template("index.html")
 
+
 @app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+def post_routes():
+    routes = None
+    data = {'destination': request.form['destination'], 'source': request.form['source']}
+    r = requests.post(api_url, data=data)
+    return render_template('index.html', routes = r.content)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='127.0.0.1', port=8080)
